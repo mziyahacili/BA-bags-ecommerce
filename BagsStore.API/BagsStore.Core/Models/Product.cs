3a016aac111ec06625278fe7f26065d25bf023fa
Ziya Hacili <mziyahajl@mail.ru>
2024-10-04 01:31:25 +0400

    API realized

---
+++ BagsStore.API/BagsStore.Core/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagsStore.Core.Models;
+
+public class Product
+{
+    public Product(Guid Id, string name, string category, string image, decimal old_coffee, deciam new_price)
+    {
+        
+    }
+
+    public Guid Id { get; }
+    public string name { get;set; }
+    public string category { get;set; }
+
+    public string image { get;set; }
+
+    public decimal old_price { get;set; }
+
+    public decimal new_price { get;set;}
+}
